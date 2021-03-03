7e88ed9a56ac9b2d2adf204bad649652c0d7f4d2
StephenWithPH <StephenWithPH@users.noreply.github.com>
2021-03-03 11:39:22 -0800

    create simple smoketest for python environment

---
+++ python-smoketest/smoketest/hello.py
@@ -0,0 +1,6 @@
+# pylint:disable=missing-module-docstring
+
+
+def say_hello(name: str) -> str:
+    """Says hello to the provided name."""
+    return f"""hello, {name}"""
---
+++ python-smoketest/tests/test_hello.py
@@ -0,0 +1,7 @@
+# pylint:disable=missing-module-docstring,missing-function-docstring
+
+import smoketest.hello as hello
+
+
+def test_say_hello_says_hello_to_provided_name():
+    assert hello.say_hello("friend") == "hello, friend"
